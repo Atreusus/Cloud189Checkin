0426d3789f299ad13b1143c264bfee86d8a3c0c8
Atreusus <70322626+Atreusus@users.noreply.github.com>
2026-06-12 19:47:11 +0800

    Update showDoc.js

--- src/push/showDoc.js
+++ src/push/showDoc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    sendKey: process.env.SHOWDOC_KEY || 'https://push.showdoc.com.cn/server/api/push/e8fe9f7458d6f8729b18b1b019fc56bb693bf39c604eaf0905662c9350af1d87',
+    sendKey: process.env.SHOWDOC_KEY || 'e8fe9f7458d6f8729b18b1b019fc56bb693bf39c604eaf0905662c9350af1d87',
 };
   
